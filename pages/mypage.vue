8ce92ef927f7f509773484283d81033513d70dc6
SoraY677 <ys4080@icloud.com>
2020-08-12 18:29:47 +0900

    mypageを追加

---
+++ pages/mypage.vue
@@ -0,0 +1,35 @@
+<template>
+ <div>
+   <div class="post-sum-container mx-auto py-3">
+   <postsum :sum="200"/>
+   </div>
+   <ul class="mypost-container mx-auto">
+     <li><mypost :username="'H.Nさん'" :postnum="20"/></li>
+   </ul>
+ </div>
+</template>
+
+<script>
+import postsum from "@/components/postSum"
+import mypost from "@/components/mypost"
+
+export default {
+  components:{
+    postsum,
+    mypost
+  }
+}
+</script>
+
+<style>
+
+.post-sum-container{
+  width:30%;
+  min-width:380px
+}
+
+.mypost-container{
+  width:85%;
+}
+
+</style>
