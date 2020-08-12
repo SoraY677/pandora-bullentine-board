9a4b5d5d410f7ddbbef7d0e104233cff6612bae2
SoraY677 <ys4080@icloud.com>
2020-08-12 17:39:08 +0900

    投稿数を表示

---
+++ components/postSum.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-card class="bg-primary text-center">
+    <p class="text-white">
+      投稿
+      <span class="sum-num">{{sum}}</span>
+      件
+    </p>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props:{
+    sum : Number
+  }
+}
+</script>
+
+<style scoped>
+.sum-num{
+  font-size:3em;
+}
+
+</style>
