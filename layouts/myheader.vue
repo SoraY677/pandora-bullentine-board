3255aef883fecb3242aca5302d44728c8996bd90
SoraY677 <ys4080@icloud.com>
2020-08-12 16:17:37 +0900

    header作成

---
+++ layouts/myheader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="p-3">
+    <b-avatar></b-avatar>
+
+    <b-row class="float-right text-center" cols="2">
+      <b-col>
+        <b-avatar ></b-avatar>
+        <span class="block">投稿</span>
+      </b-col>
+      <b-col>
+        <b-avatar ></b-avatar>
+        <span class="block">MyPage</span>
+      </b-col>
+    </b-row>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header{
+  height :6em;
+  border-bottom:1px solid #ededed
+}
+
+.block{
+  display: block;
+}
+
+</style>
