ddaa543d7cfad5c2e8ff4096126da6efafb08162
SoraY677 <ys4080@icloud.com>
2020-08-12 18:29:37 +0900

    ヘッダーにリンク

--- layouts/myheader.vue
+++ layouts/myheader.vue
@@ -1,15 +1,18 @@
 <template>
   <header class="p-3">
+    <nuxt-link to="/">
     <b-avatar></b-avatar>
-
+    </nuxt-link>
     <b-row class="float-right text-center" cols="2">
       <b-col>
-        <b-avatar ></b-avatar>
+        <b-avatar></b-avatar>
         <span class="block">投稿</span>
       </b-col>
       <b-col>
-        <b-avatar ></b-avatar>
-        <span class="block">MyPage</span>
+        <nuxt-link to="mypage">
+          <b-avatar></b-avatar>
+          <span class="block">MyPage</span>
+        </nuxt-link>
       </b-col>
     </b-row>
   </header>
@@ -20,13 +23,12 @@ export default {};
 </script>
 
 <style>
-header{
-  height :6em;
-  border-bottom:1px solid #ededed
+header {
+  height: 6em;
+  border-bottom: 1px solid #ededed;
 }
 
-.block{
+.block {
   display: block;
 }
-
 </style>
