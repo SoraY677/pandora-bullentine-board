b589cc27ee618aaf6371a61edff882e24cf0e7ae
SoraY677 <ys4080@icloud.com>
2020-08-15 12:44:17 +0900

    configを表示してみる

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,6 +3,7 @@ import "firebase/firestore";
 
 if (!firebase.apps.length) {
   const config = process.env.firebase;
+  console.log(config)
   firebase.initializeApp({
     apiKey: config.apikey,
     authDomain: config.pid + ".firebaseapp.com",
