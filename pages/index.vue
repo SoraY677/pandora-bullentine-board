b0c152098e53da890bec303479b71489f0ce6e56
SoraY677 <ys4080@icloud.com>
2020-08-15 16:49:53 +0900

    リアルタイムに増やす

--- pages/index.vue
+++ pages/index.vue
@@ -46,8 +46,9 @@ export default {
       .orderBy("timestamp", "desc")
       .limit(20)
       .onSnapshot(res => {
+        console.log(res)
+        content.splice(0)
         res.forEach(doc => {
-
           let contentLine = doc.data();
           contentLine.id = doc.id;
           content.push(contentLine);
